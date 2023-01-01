33d48a5d08bb319762eb1800c540618fc30f5a19
j-mendez <jeff@a11ywatch.com>
2023-01-01 16:18:29 +0000

    Deploying to gh-pages from @ a11ywatch/react-a11ywatch-js@9f14782286cf856dd8b3d25c2bcfcecd1f37aa16 🚀

---
+++ main.417705e10994c7e79655.manager.bundle.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[2],{446:function(module,exports,__webpack_require__){__webpack_require__(447),__webpack_require__(826),__webpack_require__(825),__webpack_require__(824),module.exports=__webpack_require__(827)},495:function(module,exports){}},[[446,3,4]]]);
